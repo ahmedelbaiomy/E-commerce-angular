57a09876da1ea53a1c5c7f52328d779c9bc7f0f9
unknown <ahmedelbaiomy40@gmail.com>
2021-05-21 11:12:52 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent} from './components/about/about.component';
+import { CheckoutComponent} from './components/checkout/checkout.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'about', component: AboutComponent},
+  {path: 'checkout', component: CheckoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    CheckoutComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
