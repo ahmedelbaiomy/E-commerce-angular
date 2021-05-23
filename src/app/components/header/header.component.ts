ba95214b195181296b6e3f43248893df43335c8c
ekhlasmohamedali <ekhlasgawish123@gmail.com>
2021-05-23 14:50:40 -0700

    add logout router

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '../_Services/user.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public u: UserService,private router:Router) { }
+  logout(){
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+     // this.router.navigateByUrl('/blog/list')
+    })
+    this.u.logout();
+
+  }
+
 
   ngOnInit(): void {
   }
