415abf7b07e4cbbb29e4db6140c03ab06f654db0
ekhlasmohamedali <ekhlasgawish123@gmail.com>
2021-05-23 17:55:42 -0700

    add login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
+
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../_Models/User';
+import { UserService } from '../_Services/user.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+     public errors: string = "";
+  profileForm = new FormGroup({
+    username: new FormControl('',[
+      Validators.required ,
+      Validators.minLength(8),
+    ]),
+    password: new FormControl('',[
+      Validators.required ,
+      Validators.minLength(8),
+    ]),
+  });
+  constructor(private UserService: UserService, private router: Router) { }
+  urlImg: string = "assets/image/signin-image.jpg";
+
+  get username(){
+     return this.profileForm.get('username') ;
+  }
+  get password(){
+    return this.profileForm.get('password') ;
+  }
+  public onSubmit() {
+    console.log(this.profileForm.value);
+    console.log(this.profileForm.get('username'))
+    console.log(this.username?.value)
+    this.UserService.login(this.username?.value, this.password?.value )
+      .subscribe(
+        result => {
+          this.UserService.token = result.token;
+          localStorage.setItem('access_token', result.token!)
+          localStorage.setItem('access_id', result._id!);
+          localStorage.setItem('access_name', result.username)
+          this.router.navigate(['/home'])
+          console.log(result)
+        },
+        err => this.errors = 'Could not authenticate'
+      );
+
+  }
 
   ngOnInit(): void {
+
   }
 
 }
