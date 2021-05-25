3fc75c34d07e71c523916494e2ca6b2510dd05e4
asmaaelalfy <noureleslam065@gmail.com>
2021-05-25 11:18:20 +0200

    home page

---
+++ src/app/_Services/products.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Products } from '../_Models/Products';
+import { Token } from '@angular/compiler/src/ml_parser/lexer';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+  public token: any = localStorage.getItem('access_token');
+
+  add(nproduct: FormData) {
+    return this.http.post<Products>("http://localhost:3000/api/products", nproduct, { headers: { authorization: this.token } });
+
+  }
+
+  getAll() {
+    return this.http.get<Products[]>("http://localhost:3000/api/products")
+
+  }
+
+ updateProduct(id:any,Product:any){
+    return this.http.put(`http://localhost:3000/api/products/${id}`,Product);
+  }
+  getProductById(id:any){
+    return this.http.get(`http://localhost:3000/api/products/${id}`)
+  }
+
+
+  
+
+  DeleteProductById(id:any){
+    return this.http.delete(`http://localhost:3000/api/products/${id}`);
+  }
+
+  constructor(private http: HttpClient) { }
+}
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Products } from '../../_Models/Products';
+import { ProductsService } from '../../_Services/products.service';
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -7,9 +14,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  currentProduct:any=null;
+  // ProductToUpdate;
+
+  nproduct: Products = new Products(" ", " "," "," ");
+  selectedFile!: File;
+  addForm!: FormGroup;
+ 
+  Product = new FormData();
+
+  constructor(public ProductsService: ProductsService, public ar:  ActivatedRoute, public route: Router, public fb: FormBuilder) { 
+    this.addForm = this.fb.group({
+      product: [''],
+     description: [''],
+      category: [''],
+
+    })
+  }
 
   ngOnInit(): void {
+    this.getProduct(this.ar.snapshot.paramMap.get('id'));
   }
+
+
+
+
+
+
+  onselect(event: any) {
+    const filelist: FileList = event.target.files;
+    this.selectedFile = filelist[0];
+
+  }
+
+  save() {
+    this.Product.append('product', this.nproduct.product)
+    this.Product.append('description', this.nproduct.description)
+
+    this.Product.append('price', this.nproduct.price);
+    this.Product.append('photo', this.selectedFile);
+    // this.Product.append('title', this.nproduct.category)
+    
+
+
+    this.ProductsService.add(this.Product).subscribe(
+      a => {
+        console.log(this.selectedFile)
+        console.log(a)
+        console.log(a.photo)
+
+        console.log("hello"+this.Product)
+      }
+    )
+    this.route.navigate(["/"]);
+  }
+
+
+
+
+
+
+  getProduct(id:any): void {
+    this.ProductsService. getProductById(id)
+      .subscribe(
+        data => {
+        this.currentProduct= data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+
+        
+  }
+
+
+
+
+  
 
 }
